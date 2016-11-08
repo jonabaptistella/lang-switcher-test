7f7875c363b9458365b8a27ef49eb4ca9f9ab923
Jonathan Baptistella <jonathan@baptistella.dk>
2016-11-08 20:44:16 -0200

    Added correct languages

---
+++ site/languages/br.php
@@ -0,0 +1,9 @@
+<?php
+
+l::set('language', 'Idioma');
+l::set('latest-projects', 'Os projetos atuais ...');
+l::set('read-more', 'leia mais');
+l::set('year', 'Ano');
+l::set('previous', 'projeto anterior');
+l::set('next', 'próximo projeto');
+l::set('made-with', 'Criado com Kirby e <b>♥</b>');
---
+++ site/languages/no.php
@@ -0,0 +1,9 @@
+<?php
+
+l::set('language', 'Språk');
+l::set('latest-projects', 'Siste prosjekter...');
+l::set('read-more', 'les mer...');
+l::set('year', 'År');
+l::set('previous', 'Forrige prosjekt');
+l::set('next', 'Neste prosjekt');
+l::set('made-with', 'Laget med Kirby og <b>♥</b>');
